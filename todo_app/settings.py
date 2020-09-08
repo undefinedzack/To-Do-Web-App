9317d503aaf95b40073d53538962d0ada0dfca7a
undefinedzack <adhney.nawghare@gmail.com>
2020-09-08 17:53:41 +0530

    adding static_url to settings.py

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+import django_heroku, os
 
 django_heroku.settings(locals())
 
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'staticfiles')
