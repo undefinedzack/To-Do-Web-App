2a2246a573e6bd22cdccb53633f8a1d95100953e
undefinedzack <adhney.nawghare@gmail.com>
2020-09-08 17:23:56 +0530

    adding heroku integration

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'todo-data',
     }
-}
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
